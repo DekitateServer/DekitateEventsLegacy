d3d878043b9afa92f244f6ea5a66d161f735cfaf
ken-kentan <ken.kentan.k2@gmail.com>
2019-12-24 21:21:45 +0900

    add CreateParkourUseCase

--- src/main/kotlin/com/dekitateserver/events/presentation/parkour/ParkourController.kt
+++ src/main/kotlin/com/dekitateserver/events/presentation/parkour/ParkourController.kt
@@ -1,7 +1,23 @@
 package com.dekitateserver.events.presentation.parkour
 
 import com.dekitateserver.events.DekitateEventsPlugin
+import com.dekitateserver.events.data.vo.ParkourId
+import com.dekitateserver.events.domain.usecase.parkour.CreateParkourUseCase
+import kotlinx.coroutines.launch
+import org.bukkit.command.CommandSender
 
 class ParkourController(plugin: DekitateEventsPlugin) {
 
+    private val pluginScope = plugin.pluginScope
+
+    private val createParkourUseCase = CreateParkourUseCase(plugin.parkourRepository)
+
+    fun create(sender: CommandSender, argParkourId: String) {
+        pluginScope.launch {
+            createParkourUseCase(
+                    sender = sender,
+                    parkourId = ParkourId(argParkourId)
+            )
+        }
+    }
 }
