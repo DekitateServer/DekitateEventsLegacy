09f9f0c6d96049669fc869d8351110586e2af16b
ken-kentan <ken.kentan.k2@gmail.com>
2019-12-26 17:23:09 +0900

    add DungeonCommand template

---
+++ src/main/kotlin/com/dekitateserver/events/presentation/dungeon/DungeonCommand.kt
@@ -0,0 +1,45 @@
+package com.dekitateserver.events.presentation.dungeon
+
+import com.dekitateserver.core.command.AbstractCommand
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+
+class DungeonCommand(
+        private val dungeonController: DungeonController
+) : AbstractCommand("dungeon") {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
+        if (args.isHelpCommand()) {
+            sender.sendMessage(MESSAGES_HELP)
+            return true
+        }
+
+        when (args.first()) {
+            else -> return false
+        }
+
+        return true
+    }
+
+    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<String>) = emptyList<String>()
+
+    companion object {
+        private val MESSAGES_HELP = arrayOf(
+                "----- DekitateEvents Dungeonコマンドヘルプ -----",
+                "| §c/dungeon join [player] [dungeonId]",
+                "| §c/dungeon complete [player] [dungeonId]",
+                "| §c/dungeon lock [dungeonId] [seconds]",
+                "| §c/dungeon unlock [dungeonId]",
+                "| §c/dungeon create [dungeonId] [name]",
+                "| §c/dungeon delete [dungeonId]",
+                "| §c/dungeon edit [dungeonId] [type] <args..>",
+                "| §c/dungeon edittype",
+                "| §c/dungeon list",
+                "| §c/dungeon info [dungeonId]",
+                "| §c/dungeon reload",
+                "| §c/dungeon help",
+                "| §7[]は必須,<>は任意,()は説明",
+                "---------------------------------------"
+        )
+    }
+}
---
+++ src/main/kotlin/com/dekitateserver/events/presentation/dungeon/DungeonController.kt
@@ -0,0 +1,6 @@
+package com.dekitateserver.events.presentation.dungeon
+
+import com.dekitateserver.events.DekitateEventsPlugin
+
+class DungeonController(plugin: DekitateEventsPlugin) {
+}
