1d37c1837df3fe3929e99a8dc8c07a0c85d72521
ken-kentan <ken.kentan.k2@gmail.com>
2019-12-26 13:58:05 +0900

    add Dungeon entity

---
+++ src/main/kotlin/com/dekitateserver/events/data/entity/Dungeon.kt
@@ -0,0 +1,35 @@
+package com.dekitateserver.events.data.entity
+
+import com.dekitateserver.events.data.vo.DungeonId
+import com.dekitateserver.events.data.vo.GachaId
+import org.bukkit.Location
+
+data class Dungeon(
+        val id: DungeonId,
+        val name: String,
+        val rewardEventTicketAmount: Int = 0,
+        val rewardGachaId: GachaId? = null,
+        val joinLocation: Location?,
+        val joinMessage: String? = "{name}§rに§c参加§rしました!",
+        val joinBroadcastMessage: String? = null,
+        val completeLocation: Location? = null,
+        val completeMessage: String? = "§dクリアおめでとう! §rまた§c挑戦§rしてね!",
+        val completeBroadcastMessage: String? = null,
+        val isEnabledCompleteSound: Boolean = true,
+        val exitLocation: Location? = null,
+        val exitMessage: String? = null,
+        val lockMessage: String? = "{name}§rは§cロック中§rです! §b{time}§rお待ちください...",
+        val lockBroadcastMessage: String? = null,
+        val unLockBroadcastMessage: String? = null
+) {
+    val formattedJoinMessage = joinMessage?.format()
+    val formattedJoinBroadcastMessage = joinBroadcastMessage?.format()
+    val formattedCompleteMessage = completeMessage?.format()
+    val formattedCompleteBroadcastMessage = completeBroadcastMessage?.format()
+    val formattedExitMessage = exitMessage?.format()
+    val formattedLockMessage = lockMessage?.format()
+    val formattedLockBroadcastMessage = lockBroadcastMessage?.format()
+    val formattedUnLockBroadcastMessage = unLockBroadcastMessage?.format()
+
+    private fun String.format() = replace("{name}", name)
+}
---
+++ src/main/kotlin/com/dekitateserver/events/data/vo/DungeonId.kt
@@ -0,0 +1,3 @@
+package com.dekitateserver.events.data.vo
+
+inline class DungeonId(val value: String)
